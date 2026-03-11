7c7bfac6cd4e63996a704f1f1cb9a3105508ef95
terminumack <110970235+terminumack@users.noreply.github.com>
2026-03-11 12:15:57 +0100

    Add meta functions and handlers to bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from handlers.exchange_admin import admin_actions, ganadores_mes, reiniciar_mes,
 from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 from handlers.calc import start_p2p, get_buy_price, get_sell_price, finish_p2p, cancel_p2p, COMPRA, VENTA, COMISION
 from handlers.exchange_admin import campaign_stats
+from handlers.calc_meta import start_meta, get_meta_buy, get_meta_roi, finish_meta, cancel_meta, M_COMPRA, M_ROI, M_COMISION
 
 # --- 1. CONFIGURACIÓN DE ZONA HORARIA ---
 TIMEZONE = pytz.timezone('America/Caracas')
@@ -307,6 +308,7 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("db_test", db_diagnostic))
     app.add_handler(p2p_conv)
     app.add_handler(CommandHandler('stats_mkt', campaign_stats))
+    app.add_handler(meta_conv)
     
     app.add_handler(ChatMemberHandler(track_my_chat_member, ChatMemberHandler.MY_CHAT_MEMBER))
     app.add_handler(CallbackQueryHandler(close_announcement, pattern="^delete_announcement$"))
