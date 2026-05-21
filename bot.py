0b730840455ff4f753ed2c698a5b7ccb44e5b2b5
terminumack <110970235+terminumack@users.noreply.github.com>
2026-05-21 15:22:23 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -98,11 +98,12 @@ async def update_price_task(context: ContextTypes.DEFAULT_TYPE):
             get_bcv_intervention(), # 🔥 NUEVO: Escaneamos la intervención al mismo tiempo
             return_exceptions=True
         )
-        logging.info(f"🛑 RESULTADO CRUDO DE INTERVENCIÓN: {interv_data}")
+        
         
         market_data = results[0]
         bcv_data = results[1]
         interv_data = results[2] # 🔥 NUEVO: Atrapamos el resultado
+        logging.info(f"🛑 RESULTADO CRUDO DE INTERVENCIÓN: {interv_data}")
 
         # 2. PROCESAR BINANCE
         if isinstance(market_data, dict):
